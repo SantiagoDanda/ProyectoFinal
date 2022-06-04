4838f2e82dd3af780e90788e52324180360ab63c
Santiago-Danda <321146650@alumno.enp.unam.mx>
2022-06-03 20:35:08 -0500

    solucioné estructura, puse sesiones

--- dynamics/php/sesion.php
+++ dynamics/php/sesion.php
@@ -1,31 +1,45 @@
 <?php
-// envía el nombre al iniciar la sesion
-    $nombre = (isset($_POST["UsuarioSes"]) && $_POST["usuarioSes"] != "") ? $_POST["usuarioSes"]: false;
+    /////////////////INICIOSESION-PAPUYISUS////////////////////
+
+    session_start();
+
+    //Esto incluye la conexión a la BD
+    include("./conexion.php");
+    $conexion = connect();
+
+    // envía el nombre y la contra al iniciar la sesion
+    $nombre = (isset($_POST["usuarioSes"]) && $_POST["usuarioSes"] != "") ? $_POST["usuarioSes"]: false;
     $contrasena = (isset($_POST["contrasena"]) && $_POST["contrasena"] != "") ? $_POST["contrasena"]: false;
 
     // hacer petición a la base de datos pidiendo la contraseña 
     // la petición se hace conforme al usuario, lo busca 
-    $constraBase = "1234567";
-    $usuario = "Jesús";
+    $peticionUsuario = "SELECT usuario from usuarios where usuario = '$nombre'";
+    $consulta = mysqli_query($conexion, $peticionUsuario);
+    $consulta = mysqli_fetch_array($consulta);
+
+    $peticionContrasena = "SELECT contrasena from usuarios where contrasena = '$contrasena' and usuario = '$nombre'";
+    $consultaContra = mysqli_query($conexion, $peticionContrasena);
+    $consultaContra = mysqli_fetch_array($consultaContra);
 
     // verifica que el usuario exista
-    if($nombre != NULL){
+    if($consulta != NULL){
         // verifica que la contraseña coincida con la del usuario
-        if($constrasena != $constraBase){
+        if($contrasena != $consultaContra[0]){
             echo '
                 <script>
                     alert("El usuario o la contraseña son icorrectos");
                     window.location.href="./../..";
                 </script>
             ';
-        }else if($constraBase == $constrasena){
+        }else if($contrasena == $consultaContra[0]){
+            $_SESSION["usuario"] = $nombre;
             echo '
                 <script>
-                    window.location.href="../../statics/templates/alumnoPrincipal.html";
+                    window.location.href="./alumnoPrincipal.php";
                 </script>
             ';
         }
-    }else if($nombre == NULL){
+    }else if($consulta == NULL){
         echo'
             <script>
                 alert("El usuario no existe, por favor registrate");
