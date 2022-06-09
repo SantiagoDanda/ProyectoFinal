b427f941be0f2cdfbdbeee73faa53483c249aab1
JesusMME <321212409@alumno.enp.unam.mx>
2022-06-09 18:15:15 -0500

    commit

--- dynamics/php/info.php
+++ dynamics/php/info.php
@@ -7,11 +7,11 @@
         $numcuenta = $_SESSION['numcuenta'];
 
         // variables que voy a enviar
-        $info = (isset($_POST["info"]) && $_POST["info"] != "") ? $_POST["info"]: NULL;
-        $grupo = (isset($_POST["grupo"]) && $_POST["grupo"] != "") ? $_POST["grupo"]: NULL;
-        $contacto = (isset($_POST["contacto"]) && $_POST["contacto"] != "") ? $_POST["contacto"]: NULL;
-+
-        $peticionInfo = "UPDATE usuarios SET grupo=$grupo, url='$url', info='$info', contacto='$contacto' WHERE numcuenta=$numcuenta";
+        $info = (isset($_POST["info"]) && $_POST["info"] != "") ? $_POST["info"]: 'sin info';
+        $grupo = (isset($_POST["grupo"]) && $_POST["grupo"] != "") ? $_POST["grupo"]: 'sin grupo';
+        $contacto = (isset($_POST["contacto"]) && $_POST["contacto"] != "") ? $_POST["contacto"]: 'sin contacto';
+
+        $peticionInfo = "UPDATE usuarios SET grupo=$grupo, info='$info', contacto='$contacto' WHERE numcuenta=$numcuenta";
         echo "<br/><br/><br/>";
         $consultaA = mysqli_query($conexion, $peticionInfo);
 
